7a893f1f7c50940276d5b65dad4d1307421a63fe
Mansour <tarafdar.mansour@gmail.com>
2021-09-19 18:30:06 +0400

    test project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-moveis');
   });
 
-  it('should render title', () => {
+  it('should render paragraph', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-moveis app is running!');
+    expect(compiled.querySelector('p')?.textContent).toContain('Hello World');
   });
 });
