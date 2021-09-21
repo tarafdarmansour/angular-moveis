3b4e998db0410020494de6584cbeab553199ee58
Mansour <tarafdar.mansour@gmail.com>
2021-09-21 20:24:34 +0400

    6. NgFor - Iterating Collections in the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,14 @@ export class AppComponent {
     return n * 2;
   }
 
-  movie = {
+  movies = [{
     title: 'Spider-Man',
     releaseDate: new Date(),
     price: 1400.99
-}
+},{
+    title: 'Moana',
+    releaseDate: new Date('2016-11-14'),
+    price: 300.99
+}]
 
 }
