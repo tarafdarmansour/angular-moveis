8ef32ba13303db38149f66fba1305ecfc62507b2
Mansour <tarafdar.mansour@gmail.com>
2021-09-21 19:59:54 +0400

    After 4. Interpolation - Displaying Variables in the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-moveis'
+  title = 'angular-moveis';
+
+  duplicatenumber(n: number) {
+    return n * 2;
+  }
+
+  movie = {
+    title: 'Spider-Man',
+    releaseDate: new Date()
+}
+
 }
