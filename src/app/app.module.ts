d9327663858a5b195294cb5d94ac15590f9c4de1
Mansour <tarafdar.mansour@gmail.com>
2021-09-26 19:51:06 +0400

    12. Template Variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { TemplatevariablesComponent } from './mytest/templatevariables/templatevariables.component';
 
 @NgModule({
-  declarations: [AppComponent, MoviesListComponent],
+  declarations: [AppComponent, MoviesListComponent, TemplatevariablesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/mytest/templatevariables/templatevariables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templatevariables',
+  templateUrl: './templatevariables.component.html',
+  styleUrls: ['./templatevariables.component.css']
+})
+export class TemplatevariablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
