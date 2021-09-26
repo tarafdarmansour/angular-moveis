11e871a981b91c79e5423c75a3cd671a896bf4d8
Mansour <tarafdar.mansour@gmail.com>
2021-09-26 10:31:50 +0400

    7. NgIf - Dynamically Showing Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.movies = [
+        {
+          title: 'Spider-Man',
+          releaseDate: new Date(),
+          price: 1400.99,
+        },
+        {
+          title: 'Moana',
+          releaseDate: new Date('2016-11-14'),
+          price: 300.99,
+        },
+      ];
+    }, 4000);
+  }
+
   title = 'angular-moveis';
+
+  movies!: { title: string; releaseDate: Date; price: number }[];
 
   duplicatenumber(n: number) {
     return n * 2;
   }
-
-  movies = [{
-    title: 'Spider-Man',
-    releaseDate: new Date(),
-    price: 1400.99
-},{
-    title: 'Moana',
-    releaseDate: new Date('2016-11-14'),
-    price: 300.99
-}]
-
 }
