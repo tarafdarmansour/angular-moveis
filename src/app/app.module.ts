533f184d144baed1c3cc76aed65f85af7e099bd5
Mansour <tarafdar.mansour@gmail.com>
2021-09-27 08:00:29 +0400

    14. Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
 import { TemplatevariablesComponent } from './mytest/templatevariables/templatevariables.component';
 import { GenericListComponent } from './utilities/generic-list/generic-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [AppComponent, MoviesListComponent, TemplatevariablesComponent, GenericListComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    MoviesListComponent,
+    TemplatevariablesComponent,
+    GenericListComponent,
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
