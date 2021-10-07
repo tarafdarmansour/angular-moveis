95b2c57e4ebbf0ac83bc103fd1790dacd5d863f4
Mansour <tarafdar.mansour@gmail.com>
2021-10-07 22:19:10 +0400

    8. Reacting to Changes in the Form - Movie Filter Component

--- src/app/movies/movie-filter/movie-filter.component.ts
+++ src/app/movies/movie-filter/movie-filter.component.ts
@@ -10,12 +10,41 @@ export class MovieFilterComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {}
 
   form!: FormGroup;
+
+  movies = [
+    {
+      title: 'Spider-Man',
+      releaseDate: new Date(),
+      price: 0,
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg',
+    },
+    {
+      title: 'Moana',
+      releaseDate: new Date(),
+      price: 0,
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_FMjpg_UX1000_.jpg',
+    },
+    {
+      title: 'Inception',
+      releaseDate: new Date(),
+      price: 0,
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg',
+    },
+  ];
+
+  originalMovies = this.movies;
+
   genres = [
     { id: 1, name: 'Drama' },
     { id: 2, name: 'Action' },
   ];
 
-  clearForm() {}
+  clearForm() {
+    this.form.reset();
+  }
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       title: '',
@@ -23,5 +52,18 @@ export class MovieFilterComponent implements OnInit {
       upcommingReleases: false,
       inTheaters: false,
     });
+
+    this.form.valueChanges.subscribe((values) => {
+      this.movies = this.originalMovies;
+      this.filterMovies(values);
+    });
+  }
+
+  filterMovies(values: any) {
+    if (values.title) {
+      this.movies = this.movies.filter(
+        (movie) => movie.title.indexOf(values.title) !== -1
+      );
+    }
   }
 }
