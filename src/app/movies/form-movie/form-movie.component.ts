83894945b42b1b45a5129fbec7d4e3f731bba88c
Mansour <tarafdar.mansour@gmail.com>
2021-10-24 10:26:42 +0400

    17. Selecting Movie Theaters

--- src/app/movies/form-movie/form-movie.component.ts
+++ src/app/movies/form-movie/form-movie.component.ts
@@ -24,8 +24,14 @@ export class FormMovieComponent implements OnInit {
     { key: 2, value: 'Action' },
     { key: 3, value: 'Comedy' },
   ];
-
   selectedGenres: multipleSelectorModel[] = [];
+
+  nonSelectedMovieTheaters: multipleSelectorModel[] = [
+    { key: 1, value: 'Agora' },
+    { key: 2, value: 'Sambil' },
+    { key: 3, value: 'Megacentro' },
+  ];
+  selectedMovieTheaters: multipleSelectorModel[] = [];
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -41,6 +47,7 @@ export class FormMovieComponent implements OnInit {
       releaseDate: '',
       poster: '',
       genresIds: '',
+      movieTheatersIds: '',
     });
 
     if (this.model !== undefined) {
@@ -58,6 +65,12 @@ export class FormMovieComponent implements OnInit {
   saveChanges() {
     const genresIds = this.selectedGenres.map((value) => value.key);
     this.form.get('genresIds')?.setValue(genresIds);
+
+    const movieTheatersIds = this.selectedMovieTheaters.map(
+      (value) => value.key
+    );
+    this.form.get('movieTheatersIds')?.setValue(movieTheatersIds);
+
     this.onSaveChanges.emit(this.form.value);
   }
 }
